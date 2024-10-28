fcb3fdd3395a53107780ae8624f06d2bd8a17b51
PedroVGelli <kaicribeiro015@gmail.com>
2024-10-28 20:38:36 -0300

    Interface principal concluída

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -15,6 +14,18 @@ app.get('/aprenda', (req, res) => {
 
 app.get('/quiz', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'quiz.html'));
+});
+
+app.get('/anemia', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'anemia.html'));
+});
+
+app.get('/fenilcetonuria', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'fenilcetonuria.html'));
+});
+
+app.get('/fibrose-cistica', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'fibrose_cistica.html'));
 });
 
 app.listen(3000, () => {
